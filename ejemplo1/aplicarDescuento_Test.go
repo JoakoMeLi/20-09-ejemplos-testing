b906840590d5e05c62538afa5826ce3bc0d57a1e
JoakoMeLi <joaquin.vasquez@mercadolibre.com>
2022-09-20 15:53:15 -0300

    Primer Ejemplo

---
+++ ejemplo1/aplicarDescuento_Test.go
@@ -0,0 +1,21 @@
+package main_test
+
+import "testing"
+
+func TestDiscountApplied(t *testing.T) {
+	calculator := NewDiscountCalculator(100, 20)
+	amount := calculator.Calculate(150)
+
+	if 130 != amount {
+		t.Fail()
+	}
+}
+
+func TestDiscountNotApplied(t *testing.T) {
+	calculator := NewDiscountCalculator(100, 20)
+	amount := calculator.Calculate(50)
+
+	if 50 != amount {
+		t.Fail()
+	}
+}
---
+++ ejemplo1/go.mod
@@ -0,0 +1,3 @@
+module github.com/JoakoMeLi/20-09-ejemplos-testing/ejemplo1
+
+go 1.19
